ad20ba1bd1c2fe390552169d42412902b6ecda8d
marcodevops2 <devops2@itaas.com.mx>
2025-11-13 15:25:46 -0600

    Enable web analytics in Vercel adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
     integrations: [react(), tailwind()],
     output: 'server',
-    adapter: vercel()
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true
+        }
+    })
 });
